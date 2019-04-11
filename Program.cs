f5d4756658fec89f88be011db3336f976a665943
CloudRex <cloudrex@outlook.com>
2019-04-11 19:13:08 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LLVMSharp;
+
+namespace LlvmSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
