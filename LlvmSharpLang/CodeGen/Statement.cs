4634bbc1851faef1fec4e61e734b970f7d449b31
CloudRex <cloudrex@outlook.com>
2019-04-15 21:26:46 -0400

    Create Lexer class

--- LlvmSharpLang/CodeGen/Statement.cs
+++ LlvmSharpLang/CodeGen/Statement.cs
@@ -1,5 +1,14 @@
-namespace LlvmSharpLang.CodeGen {
-    public class Statement : IEntity<LLVM {
+using System;
+using LLVMSharp;
+using LlvmSharpLang.CodeGen.Structure;
 
+namespace LlvmSharpLang.CodeGen
+{
+    public class Statement : IEntity<LLVMValueRef, LLVMBuilderRef>
+    {
+        public LLVMValueRef Emit(LLVMBuilderRef context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
