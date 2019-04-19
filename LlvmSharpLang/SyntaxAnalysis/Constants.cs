7a82160b77ce976634aec76c003af9115c8a8d50
CloudRex <cloudrex@outlook.com>
2019-04-18 23:45:35 -0400

    Add missing complex token types for literals

--- LlvmSharpLang/SyntaxAnalysis/Constants.cs
+++ LlvmSharpLang/SyntaxAnalysis/Constants.cs
@@ -43,7 +43,11 @@ namespace LlvmSharpLang.SyntaxAnalysis
 
         public static readonly ComplexTokenTypeMap complexTokenTypes = new ComplexTokenTypeMap
         {
-            {Util.CreateRegex(@"[_a-z]+[_a-z0-9]*"), TokenType.Id}
+            {Util.CreateRegex(@"[_a-z]+[_a-z0-9]*"), TokenType.Id},
+            {Util.CreateRegex("\"(\\.|[^\"\\])*\""), TokenType.LiteralString},
+            {Util.CreateRegex(@"[0-9]+"), TokenType.LiteralInteger},
+            {Util.CreateRegex(@"[0-9]+\.[0-9]+"), TokenType.LiteralDecimal},
+            {Util.CreateRegex(@"'([^'\\\n]|\\.)'"), TokenType.LiteralCharacter}
         };
     }
 
