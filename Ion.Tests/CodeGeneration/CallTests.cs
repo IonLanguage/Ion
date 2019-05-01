ae424c7ef3c03785ed703aedd167216ac16b0953
CloudRex <cloudrex@outlook.com>
2019-04-30 20:58:31 -0400

    Add function call test

---
+++ Ion.Tests/CodeGeneration/CallTests.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using LLVMSharp;
+using Ion.SyntaxAnalysis;
+using NUnit.Framework;
+using Ion.Abstraction;
+using Ion.Tests.Core;
+using Ion.CodeGeneration;
+using Ion.Parsing;
+using Ion.Core;
+
+namespace Ion.Tests.CodeGeneration
+{
+    [TestFixture]
+    internal sealed class CallTests
+    {
+        private Abstraction.Module module = new Ion.Abstraction.Module();
+
+        [SetUp]
+        public void Setup()
+        {
+            // Create a new LLVM module instance.
+            this.module = new Ion.Abstraction.Module();
+
+            // Reset symbol table along with its functions.
+            SymbolTable.Reset();
+            SymbolTable.functions.Clear();
+        }
+
+        [Test]
+        public void CallWithoutArguments()
+        {
+            // Create the token stream.
+            TokenStream stream = new TokenStream(new Token[]
+            {
+                // Program starting point token.
+                new Token {
+                    Type = TokenType.Unknown
+                },
+
+                new Token {
+                    Type = TokenType.Identifier,
+                    Value = "main"
+                },
+
+                new Token {
+                    Type = TokenType.SymbolParenthesesL,
+                    Value = "("
+                },
+
+                new Token {
+                    Type = TokenType.SymbolParenthesesR,
+                    Value = ")"
+                },
+
+                new Token {
+                    Type = TokenType.SymbolSemiColon,
+                    Value = ";"
+                }
+            });
+
+            // Read the expected output IR code.
+            string expected = File.ReadAllText(TestUtil.ResolveDataPath("CallWithoutArguments.ll"));
+
+            // Emit the main function.
+            this.module.CreateMainFunction();
+
+            // Emit the module.
+            string output = this.module.ToString();
+
+            // Compare stored IR code with the actual, emitted output.
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
