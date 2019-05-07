d56d351ed0da4cf4f3ad22ccc89e646c7758f032
CloudRex <cloudrex@outlook.com>
2019-05-06 21:01:36 -0400

    Improve string expr parser

--- Ion/Parsing/StringExprParser.cs
+++ Ion/Parsing/StringExprParser.cs
@@ -9,25 +9,24 @@ namespace Ion.Parsing
     {
         public StringExpr Parse(TokenStream stream)
         {
-            // Consume string literal token.
+            // Ensure current token is string literal.
+            stream.EnsureCurrent(TokenType.LiteralString);
+
+            // Capture string literal token.
             Token token = stream.Get();
 
             // Skip string literal token.
             stream.Skip();
 
-            // Ensure captured token is a string.
-            if (token.Type != TokenType.LiteralString)
-            {
-                throw new Exception($"Expected token type to be string, but got '{token.Type}'");
-            }
-
+            // TODO: Hard-coded.
+            // Remove string quotes.
             string value = token.Value.Substring(1, token.Value.Length - 2);
 
-            // Create the numeric expression entity.
-            StringExpr numericExpr = new StringExpr(token.Type, Resolvers.TypeFromToken(token), value);
+            // Create the string expression entity.
+            StringExpr stringExpr = new StringExpr(token.Type, Resolvers.TypeFromToken(token), value);
 
-            // Return the numeric expression entity.
-            return numericExpr;
+            // Return the string expression entity.
+            return stringExpr;
         }
     }
 }
