dab1dc501a7adbfbe9fb10cbeb75a1ab1e78aadc
draem <draemwoo@gmail.com>
2019-05-11 16:00:17 -0400

    Use String.join instead of for loop for path.

--- Ion/Parsing/Helpers/PathResult.cs
+++ Ion/Parsing/Helpers/PathResult.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
@@ -15,19 +16,8 @@ namespace Ion.Parsing
 
         public override string ToString()
         {
-            // Create the string builder instance.
-            StringBuilder builder = new StringBuilder();
-
-            foreach (string node in this.nodes)
-            {
-                builder.Append($"{node}.");
-            }
-
-            // Retrieve the string builder's result.
-            string result = builder.ToString();
-
-            // Trim the ending, extra dot character.
-            result = result.TrimEnd('.');
+            // Creates a string represention of the path.
+            string result = String.Join(".", nodes);
 
             // Return the resulting string.
             return result;
