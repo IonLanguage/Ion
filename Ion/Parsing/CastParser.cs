bfabcad88486b42745d5c74a07353fe42f53b80c
CloudRex <cloudrex@outlook.com>
2019-05-21 15:41:23 -0400

    Implement CastParser

--- Ion/Parsing/CastParser.cs
+++ Ion/Parsing/CastParser.cs
@@ -1,4 +1,29 @@
+using Ion.CodeGeneration;
+using Ion.SyntaxAnalysis;
+
 namespace Ion.Parsing
 {
-    // TODO
+    public class CastParser : IParser<Type>
+    {
+        public Type Parse(ParserContext context)
+        {
+            // Ensure current token is parentheses start.
+            context.Stream.EnsureCurrent(TokenType.SymbolParenthesesL);
+
+            // Skip parentheses start token.
+            context.Stream.Skip();
+
+            // Invoke type parser.
+            Type target = new TypeParser().Parse(context);
+
+            // Ensure current token is parentheses end.
+            context.Stream.EnsureCurrent(TokenType.SymbolParenthesesR);
+
+            // Skip parentheses end token.
+            context.Stream.Skip();
+
+            // Return the resulting target type.
+            return target;
+        }
+    }
 }
