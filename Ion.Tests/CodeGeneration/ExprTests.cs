e8195ce2f244426ac8539dcca54defae3d99eeb3
CloudRex <cloudrex@outlook.com>
2019-06-06 16:31:02 -0400

    Fix tests referencing renamed method

--- Ion.Tests/CodeGeneration/ExprTests.cs
+++ Ion.Tests/CodeGeneration/ExprTests.cs
@@ -18,7 +18,7 @@ namespace Ion.Tests.CodeGeneration
         public static void Setup()
         {
             // Reset the name counter before every test.
-            NameRegister.ResetAll();
+            NameRegister.Reset();
         }
 
         [Test]
