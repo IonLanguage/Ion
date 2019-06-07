47822792bf1c2f34229c47b525e01252d11e94f9
CloudRex <cloudrex@outlook.com>
2019-06-06 23:18:55 -0400

    Allow directives to be declared without a value

--- Ion/Parsing/DirectiveParser.cs
+++ Ion/Parsing/DirectiveParser.cs
@@ -1,4 +1,5 @@
 using Ion.CodeGeneration;
+using Ion.CognitiveServices;
 using Ion.Misc;
 using Ion.SyntaxAnalysis;
 
@@ -17,11 +18,24 @@ namespace Ion.Parsing
             // Invoke path parser to capture option key.
             PathResult key = new PathParser().Parse(context);
 
-            // Capture string literal token.
-            string value = context.Stream.Get(TokenType.LiteralString).Value;
+            // Create the value buffer.
+            string value = null;
 
-            // Remove the quotes from the value.
-            value = Util.ExtractStringLiteralValue(value);
+            // Capture the current token's type.
+            TokenType currentTokenType = context.Stream.Current.Type;
+
+            // Directive value is set, process it.
+            if (TokenIdentifier.IsLiteral(context.Stream.Current.Type))
+            {
+                // Capture string literal token.
+                value = context.Stream.Current.Value;
+
+                // Remove the single/double quotes from the value if applicable.
+                if (context.Stream.Current.Type == TokenType.LiteralString || currentTokenType == TokenType.LiteralCharacter)
+                {
+                    value = Util.ExtractStringLiteralValue(value);
+                }
+            }
 
             // Skip string literal token.
             context.Stream.Skip();
